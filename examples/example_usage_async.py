97c14aebe5fb1a9f8567bf4052966ce8ccf9717e
Tib612 <thibaut-c@hotmail.com>
2024-07-19 11:03:38 +0200

    Update example_usage_async.py

--- examples/example_usage_async.py
+++ examples/example_usage_async.py
@@ -12,10 +12,19 @@ async def main():
 
     balance = await pc.get_balance()
     print(balance)
+    # Will return a PluxeeBalance object with those attributes
+    # lunch_pass: 89.19
+    # eco_pass: 396.16
+    # gift_pass: 0.0
+    # conso_pass: 0.0
 
     transactions = await pc.get_transactions(PassType.LUNCH, date(2024, 1, 25), date(2024, 3, 1))
     print(transactions)
-
+    # Will return a list of PluxeeTransaction object with those attributes
+    # date: 2024-06-19
+    # amount: -75.24
+    # detail: Paiement classique
+    # merchant: Colruyt Food Retail
 
 if __name__ == "__main__":
     asyncio.run(main())
