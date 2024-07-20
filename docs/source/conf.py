0ad27bdbe3bb11b470b2278d449c68510e89f488
Thibaut Capuano <tib612@PC-TIB>
2024-07-20 10:51:03 +0200

    bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'pluxee-api'
 copyright = '2024, Thibaut Capuano'
 author = 'Thibaut Capuano'
-release = '0.1.4'
+release = '0.1.5'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
